4a4740651c4ff0e635441651e21d31efefdb5b14
kbrito504 <kbrito@flashvolve.com>
2024-11-04 09:41:36 -0300

    CHORE: make i18n pt variables message for home page

---
+++ src/i18n/locales/pt/pages.js
@@ -0,0 +1,15 @@
+export default {
+    home: {
+        title: 'Bem-vindo à Flash Automação Criativa',
+        description: 'Remova o fundo de suas imagens de forma fácil e rápida. Basta fazer o upload da sua imagem abaixo.',
+        upload: 'Clique ou arraste e solte a imagem aqui para fazer o upload.',
+        download: 'Download',
+        list_title: 'Personalizações Anteriores',
+        select_all: 'Selecionar Todas',
+        customization_fields: {
+            campaign: 'Campanha:',
+            phone: 'Telefone:',
+            company: 'Empresa:',
+        }
+    }
+};
