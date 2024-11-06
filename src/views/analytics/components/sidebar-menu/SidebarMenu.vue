9cbb4680376ce2c177cf2ef93c74f7f0057f56a7
kbrito504 <kbrito@flashvolve.com>
2024-11-06 08:24:31 -0300

    CHORE: include settings example route and remove path link from others ilustration options

--- src/views/analytics/components/sidebar-menu/SidebarMenu.vue
+++ src/views/analytics/components/sidebar-menu/SidebarMenu.vue
@@ -122,7 +122,7 @@ const adminMenuOptions = computed(() => [
     {
         label: t('menu.users'),
         key: 'dashboard/admin/users',
-        key: '/dashboard/admin/users',
+        key: '#',
         icon: () => h('svg', { 
             'xmlns': 'http://www.w3.org/2000/svg',
             'viewBox': '0 0 24 24',
@@ -137,7 +137,7 @@ const adminMenuOptions = computed(() => [
     },
     {
         label: t('menu.organizations'),
-        key: '/dashboard/admin/organizations',
+        key: '#',
         icon: () => h('svg', { 
             'xmlns': 'http://www.w3.org/2000/svg',
             'viewBox': '0 0 24 24',
@@ -146,6 +146,24 @@ const adminMenuOptions = computed(() => [
             'height': '24px',
         }, [
             h('path', { d: 'M18 15h-2v2h2m0-6h-2v2h2m2 6h-8v-2h2v-2h-2v-2h2v-2h-2V9h8M10 7H8V5h2m0 6H8V9h2m0 6H8v-2h2m0 6H8v-2h2M6 7H4V5h2m0 6H4V9h2m0 6H4v-2h2m0 6H4v-2h2m6-10V3H2v18h20V7z' }),
+        ]),
+    },
+    {
+        label: t('menu.settings'),
+        key: '#',
+        icon: () => h('svg', { 
+            'xmlns': 'http://www.w3.org/2000/svg',
+            'viewBox': '0 0 24 24',
+            'fill': 'none',
+            'stroke': 'currentColor',
+            'stroke-linecap': 'round',
+            'stroke-linejoin': 'round',
+            'stroke-width': '2',
+            'width': '24px',
+            'height': '24px',
+        }, [
+            h('path', { d: 'M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066-2.573c-.94-1.543.826-3.31 2.37-2.37c.996.608 2.296.07 2.572-1.065' }),
+            h('path', { d: 'M15 12a3 3 0 1 1-6 0a3 3 0 0 1 6 0' }),
         ]),
     },
 ]);
