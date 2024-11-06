a6e86184407227bbc381466c9e928282d9524ff7
kbrito504 <kbrito@flashvolve.com>
2024-11-06 10:47:44 -0300

    CHORE: make english i18n variables messages files

---
+++ src/i18n/locales/en/menu.js
@@ -0,0 +1,17 @@
+export default {
+    light_theme: 'Light Theme',
+    dark_theme: 'Dark Theme',
+    
+    dashboard: 'Dashboard',
+    uploader: 'Uploader',
+    history: 'History',
+    users: 'Users',
+    organizations: 'Organizations',
+    settings: 'Settings',
+
+    theme: 'Theme',
+    language: 'Language',
+
+    welcome: 'Hello, {name}! 👋',
+    profile: 'Profile',
+};
---
+++ src/i18n/locales/en/pages.js
@@ -0,0 +1,15 @@
+export default {
+    home: {
+        title: 'Welcome to Flash Creative Automation',
+        description: 'Easily and quickly remove the background from your images. Just upload your image below.',
+        upload: 'Click or drag and drop the image here to upload.',
+        download: 'Download',
+        list_title: 'Previous Customizations',
+        select_all: 'Select All',
+        customization_fields: {
+            campaign: 'Campaign:',
+            phone: 'Phone:',
+            company: 'Company:',
+        }
+    }
+};
